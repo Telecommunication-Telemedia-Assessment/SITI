04db3d7ebcc6b180840a0413a2a29a8fa7285282
Stephan Fremerey <stephan.fremerey@tu-ilmenau.de>
2018-08-07 17:38:45 +0200

    Bugfix in computing SI/TI values for multiplot script.

--- examples/multiplot_SITI.py
+++ examples/multiplot_SITI.py
@@ -2,20 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 videos_list = []
-for i in range(1, 8):
+for i in range(1, 10):
   videos_list.append(i)
 
-SI_TI_mean = pd.DataFrame()
+SI_TI = pd.DataFrame()
 
 for v in videos_list:
   df = pd.read_csv("%s.txt" % (v))
-  SI_TI_mean[v-1] = [df["SI"].mean(), df["TI"].mean()]
+  SI_TI[v-1] = [df["SI"].max(), df["TI"].max()]
 
-SI_TI_mean = SI_TI_mean.T
-SI_TI_mean.columns = ["Mean SI", "Mean TI"]
+SI_TI = SI_TI.T
+SI_TI.columns = ["SI", "TI"]
 
-ax = SI_TI_mean.plot(style='o')
+ax = SI_TI.plot(style='o')
 ax.set_xticklabels(videos_list)
 plt.xlabel("Content")
-plt.ylabel("Mean SI/TI")
-plt.savefig("SITI_analysis.jpg", dpi=300, bbox_inches='tight')
+plt.ylabel("SI/TI")
+plt.savefig("SITI_analysis.png", dpi=300, bbox_inches='tight')
